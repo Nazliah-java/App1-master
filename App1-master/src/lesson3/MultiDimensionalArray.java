f78c71a0b8c79755ba0e1c1f2e34476f95056e7c
Nazliah-java <nazliahborhan5@gmail.com>
2024-02-08 16:04:20 +0800

    FirstCommit

--- App1-master/src/lesson3/MultiDimensionalArray.java
+++ App1-master/src/lesson3/MultiDimensionalArray.java
@@ -3,16 +3,16 @@ package lesson3;
 public class MultiDimensionalArray {
     public static void main(String[] args) { 
         int [][][] a = {{{1,2,3},{4,5},{6}},{{7,8},{9}}}; 
-   System.out.println("a.length = "+a.length); 
+        System.out.println("a.length = "+a.length); //print the length of dimensional array
    
 
-    for (int i = 0; i < a.length; i++){
+    for (int i = 0; i < a.length; i++){ //outer loops
         System.out.println("a[" + i +"].length = " + a[i].length);
 
-    for (int j = 0; j < a[i].length; j++){
+    for (int j = 0; j < a[i].length; j++){ //middle loops
         System.out.println("a[" + i + "][" + j + "].length = " + a[i][j].length);
 
-    for (int k = 0; k < a[i][j].length; k++){
+    for (int k = 0; k < a[i][j].length; k++){ //inner loops
         System.out.print("a[" + i +"][" + j + "][" + k + "] = " + a[i][j][k] + ", ");
     }
     System.out.println();
