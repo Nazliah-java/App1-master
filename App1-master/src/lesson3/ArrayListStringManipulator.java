bc6c49cc64e92c07addd181a62535191a1357dc8
Nazliah-java <nazliahborhan5@gmail.com>
2024-02-08 15:53:36 +0800

    First Commit

---
+++ App1-master/src/lesson3/ArrayListStringManipulator.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+import java.util.ArrayList;
+
+public class ArrayListStringManipulator {
+
+    private ArrayList<String> arrayList;
+    
+    public ArrayListStringManipulator() {
+        arrayList = new ArrayList<>();
+}
+
+}
